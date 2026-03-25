bb293b26e3217090a5d7bf37b44a6efcbe7dc7d0
vaishnavivasireddi2006-maker <vaishnavivasireddi2006@gmail.com>
2026-03-25 16:25:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,19 @@
 import streamlit as st
 import pandas as pd
 import string
+import re
+
+def clean_text(text):
+    text = str(text).lower()
+    text = re.sub(r'[^a-z\s]', '', text)
+    return text
 
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.naive_bayes import MultinomialNB
 
 # Load dataset
 df = pd.read_csv("customer_support_tickets.csv")
-st.write(df.columns)
+st.write(df["Ticket Type"].value_counts())   # 👈 check imbalance
 
 # Clean text
 def clean_text(text):
@@ -18,13 +24,16 @@ def clean_text(text):
 df["clean_text"] = df["Ticket Description"].apply(clean_text)
 
 # Train model inside app
-vectorizer = TfidfVectorizer()
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.linear_model import LogisticRegression
+
+vectorizer = TfidfVectorizer(stop_words='english', max_features=5000)
 X = vectorizer.fit_transform(df["clean_text"])
+
 y = df["Ticket Type"]
 
-model = MultinomialNB()
+model = LogisticRegression(max_iter=1000)
 model.fit(X, y)
-
 # UI
 st.title("🎫 Support Ticket Classifier")
 
